412c203c5967d4d3f506cb7e4fc6f78eb3e969cc
nero <wnsgur765z@naver.com>
2022-04-17 23:24:52 +0900

    feat: Add 순수함수와 부수효과

--- ch01/code1-3.js
+++ ch01/code1-3.js
@@ -12,3 +12,21 @@ function showStudent(ssn) {
 }
 
 showStudent('444-444-444');
+
+//refactoring
+
+var find = curry((db, id) => {
+  let obj = db.find(id);
+  if (obj === null) {
+    throw new Error('객체를 찾을 수 없습니다');
+  }
+  return obj;
+});
+// 검색할 학생 id 와 객체를 찾는다
+
+var csv = (student) =>
+  `${student.ssn}, ${student.firstname} ${studentlastname}`;
+
+var append = curry((selector, info) => {
+  document.querySelector(selector).innerHTML = info;
+});
